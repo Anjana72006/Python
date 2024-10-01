1a4aadcc281a7f4efc4195e82cfe2c46ac3c9a3e
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2024-10-01 15:39:40 +0530

    Add files via upload

---
+++ jjjj.py
@@ -0,0 +1,17 @@
+#Python Program to get Student details
+#Author=Sarju S
+#Date=October 1,2024
+
+print('I Love Phython')
+
+name=input('Enter your student name:')
+roll_no=int(input('Enter your roll number:'))
+cgpa=float(input('Enter your cgpa here:'))
+cgpa_percentage=cgpa*10
+
+
+
+print('Your name is',name)
+print('Your roll number is',roll_no)
+print('Your cgpa is',cgpa)
+print('Your cgpa percentage is',cgpa percentage)
