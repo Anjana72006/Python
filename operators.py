69e5a4cd10014af86b128c81f3816297d380df24
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2024-10-06 10:43:15 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,12 @@
+#progrm using operators
+#Author Anjana Krishna
+#Date   5 october 2024
+a=10
+b=5
+Sum=a+b
+Division=a/b
+print("Sum:",Sum,",Division:",Division,)
+print("Is a greater than b?:",a>b,)
+print("Are a and b equal?:",a==b,)
+print("Logical AND:",a>b and b<a)
+print("Logical OR:",a>b or a==b)
