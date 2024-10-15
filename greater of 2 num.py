0631fd1018f84c41676ff14e68ea14942877a5e4
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2024-10-15 14:30:36 +0530

    Add files via upload

---
+++ greater of 2 num.py
@@ -0,0 +1,12 @@
+#Author Anjana Krishna
+#Date 14 oct 2024
+# Python program to check greater of two number
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+if num1>num2:
+    print("num1 is greater")
+elif num2>num1:
+    print("num2 is greater")
+else:
+    print("num1 equal to num2")
+
