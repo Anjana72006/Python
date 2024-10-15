9ac4e7c657f7b3fbdc44a173ea368545608a3897
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2024-10-15 14:08:23 +0530

    Add files via upload

--- positive number.py
+++ positive number.py
@@ -5,4 +5,4 @@ number=int(input("Enter a number:"))
 if number>0:
     print("The given number",number,"is positive")
 else:
-    print("The given number is negative")
+    print("The given number ",number,"is negative")
