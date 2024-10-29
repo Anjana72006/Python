48d70d9d48b708b8d153027eef903f3e42c7740b
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2024-10-29 15:20:13 +0530

    Add files via upload

---
+++ multiplication table.py
@@ -0,0 +1,6 @@
+#AuthOR Anjana Krishna
+#Date 29 oct 2024
+#program to print multiplication table of a number upto 12
+number=int(input("Enter a number:"))
+for i in range(1,13):
+    print(f"{number}* {i}\t={number*i}")
