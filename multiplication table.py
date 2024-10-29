7bbc14873ef7d35261fb7d84ff68b21234a029eb
Anjana72006 <anjanakrishna2028@cs.sjcetpalai.ac.in>
2024-10-29 16:04:18 +0530

    Add files via upload

--- multiplication table.py
+++ multiplication table.py
@@ -2,5 +2,6 @@
 #Date 29 oct 2024
 #program to print multiplication table of a number upto 12
 number=int(input("Enter a number:"))
-for i in range(1,13):
+limit=int(input ("Enter your limit:"))
+for i in range(1,limit+1):
     print(f"{number}* {i}\t={number*i}")
